e9845c5e3c1af003ce1566876c00e23d5fa8ad90
GliGli <sfx.gligli@free.fr>
2015-09-29 21:16:06 +0200

    storage: useful default vibrato speed

--- firmware_17xx/synth/storage.c
+++ firmware_17xx/synth/storage.c
@@ -401,6 +401,7 @@ LOWERCODESIZE void preset_loadDefault(int8_t makeSound)
 	currentPreset.continuousParameters[cpLFOPitchAmt]=UINT16_MAX/16;
 	currentPreset.continuousParameters[cpLFOFreq]=HALF_RANGE;
 	currentPreset.continuousParameters[cpAmpSus]=UINT16_MAX;
+	currentPreset.continuousParameters[cpVibFreq]=8*UINT16_MAX/10+1;
 
 	currentPreset.steppedParameters[spBenderRange]=1;
 	currentPreset.steppedParameters[spBenderTarget]=modPitch;
