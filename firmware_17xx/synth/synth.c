692f3228ed511ae14daa862c215ebc0bf4d8099f
GliGli <sfx.gligli@free.fr>
2015-10-07 23:23:47 +0200

    synth: further improvements to VCA linearity

--- firmware_17xx/synth/synth.c
+++ firmware_17xx/synth/synth.c
@@ -617,7 +617,7 @@ static FORCEINLINE uint16_t adjustCV(cv_t cv, uint32_t value)
 		value=UINT16_MAX-value;
 		break;
 	case cvAmp:
-		value=(9*value)/16; // limit VCA output level to 4Vpp
+		value=(6*value)/16; // limit VCA output level to 4Vpp
 		value=computeShape(value<<8,vcaLinearizationCurve,1);		
 		break;
 	case cvNoiseVol:
