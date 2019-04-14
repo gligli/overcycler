03f8db975b5657b9152ad1a1021b6a357a2bb0d9
GliGli <sfx.gligli@free.fr>
2019-04-14 08:54:42 +0200

    fix VCnt param display

--- firmware_17xx/synth/ui.c
+++ firmware_17xx/synth/ui.c
@@ -128,7 +128,7 @@ const struct uiParam_s uiParameters[9][2][10] = // [pages][0=pots/1=keys][pot/ke
 			{.type=ptCont,.number=cpGlide,.shortName="Glid",.longName="Glide amount"},
 			{.type=ptCont,.number=cpUnisonDetune,.shortName="MDet",.longName="Master unison Detune"},
 			{.type=ptCont,.number=cpMasterTune,.shortName="MTun",.longName="Master Tune"},
-			{.type=ptStep,.number=spVoiceCount,.shortName="VCnt",.longName="Voice count",.values={"1","2","3","4","5","6"}},
+			{.type=ptStep,.number=spVoiceCount,.shortName="VCnt",.longName="Voice count",.values={"   1","   2","   3","   4","   5","   6"}},
 			{.type=ptCont,.number=cpNoiseVol,.shortName="NVol",.longName="Noise Volume"},
 			/* 2nd row of pots */
 			{.type=ptCont,.number=cpAmpAtt,.shortName="AAtk",.longName="Amplifier Attack"},
