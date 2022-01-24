e7ed16ec8d439521699acb74560295033455adc7
GliGli <sfx.gligli@free.fr>
2022-01-24 19:35:44 +0100

    uart_midi: init/ack clarification

--- firmware_17xx/synth/uart_midi.c
+++ firmware_17xx/synth/uart_midi.c
@@ -6,6 +6,7 @@
 
 __attribute__ ((used)) void UART2_IRQHandler(void)
 {
+	LPC_UART2->IIR; // acknowledge interrupt
 	synth_uartEvent(UART_ReceiveData(LPC_UART2));
 }
 
@@ -21,9 +22,14 @@ void uartMidi_init(void)
 
 	UART_FIFO_CFG_Type fifo;
 	UART_FIFOConfigStructInit(&fifo);
+	fifo.FIFO_Level=UART_FIFO_TRGLEV0;
 	UART_FIFOConfig((LPC_UART_TypeDef*)LPC_UART2,&fifo);
 	
 	UART_IntConfig(LPC_UART2,UART_INTCFG_RBR,ENABLE);
+	UART_IntConfig(LPC_UART2,UART_INTCFG_THRE,DISABLE);
+	UART_IntConfig(LPC_UART2,UART_INTCFG_RLS,DISABLE);
+	UART_IntConfig(LPC_UART2,UART_INTCFG_ABEO,DISABLE);
+	UART_IntConfig(LPC_UART2,UART_INTCFG_ABTO,DISABLE);
 
 	NVIC_SetPriority(UART2_IRQn,2);
 	NVIC_EnableIRQ(UART2_IRQn);
