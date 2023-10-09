0fc2ab8d9bb83b8f1bddfb427f1563bd7af46e1d
GliGli <sfx.gligli@free.fr>
2023-10-09 15:26:55 +0200

    arp: resync with Imogen's p600fw

--- firmware_17xx/synth/arp.c
+++ firmware_17xx/synth/arp.c
@@ -12,6 +12,8 @@
 
 #define ARP_NOTE_MEMORY 128 // must stay>=128 for up/down mode
 
+// this flag is used to indicate which of the current arp notes are latched so that these can be
+// removed when latch mode is deactivated while the currently held keys continue to be played:
 #define ARP_NOTE_HELD_FLAG 0x80
 
 #define ARP_LAST_NOTE (ARP_NOTE_MEMORY-1)
@@ -20,6 +22,7 @@ static struct
 {
 	uint8_t notes[ARP_NOTE_MEMORY];
 	int16_t noteIndex;
+	int16_t previousIndex; // this is used to avoid multiple successive playing of notes (or indices) in random arp
 	uint8_t previousNote;
 	int8_t transpose,previousTranspose;
 
@@ -42,7 +45,7 @@ static void finishPreviousNote(void)
 {
 	if(arp.previousNote!=ASSIGNER_NO_NOTE)
 	{
-		uint8_t n=arp.previousNote&~ARP_NOTE_HELD_FLAG;
+		uint8_t n=arp.previousNote&~ARP_NOTE_HELD_FLAG; // remove the HELD bit
 		
 		assigner_assignNote(n+SCANNER_BASE_NOTE+arp.previousTranspose,0,0,0);
 	}
@@ -53,6 +56,7 @@ static void killAllNotes(void)
 	finishPreviousNote();
 
 	arp.noteIndex=-1;
+	arp.previousIndex=-1;
 	arp.previousNote=ASSIGNER_NO_NOTE;
 
 	memset(arp.notes,ASSIGNER_NO_NOTE,ARP_NOTE_MEMORY);
@@ -75,15 +79,21 @@ static void killHeldNotes(void)
 inline void arp_setMode(arpMode_t mode, int8_t hold)
 {
 	// stop previous assigned notes
-	
+
 	if(mode!=arp.mode)
 	{
-		killAllNotes();
-		
-		if (mode!=amOff)
-			arp_resetCounter(settings.syncMode==smInternal);
+		if ((mode==amRandom && arp.mode==amAssign) || (arp.mode==amRandom && mode==amAssign))
+		{
+			// any action here?
+		}
+		else
+		{
+			killAllNotes();
+			if (mode!=amOff)
+				arp_resetCounter(settings.syncMode==smInternal);
+		}
 	}
-	
+
 	if(!hold && arp.hold)
 		killHeldNotes();
 
@@ -125,10 +135,9 @@ void arp_assignNote(uint8_t note, int8_t on)
 	note-=SCANNER_BASE_NOTE;
 	if(on)
 	{
+
 		// if this is the first note, make sure the arp will start on it as as soon as we update
-		
-		if(isEmpty())
-			arp_resetCounter(settings.syncMode==smInternal);
+		if(isEmpty()) arp_resetCounter(settings.syncMode==smInternal);
 
 		// assign note			
 		
@@ -197,7 +206,7 @@ void arp_assignNote(uint8_t note, int8_t on)
 
 void arp_update(void)
 {
-	uint8_t n;
+	uint8_t n, step;
 	
 	// arp off -> nothing to do
 	
@@ -217,20 +226,38 @@ void arp_update(void)
 	
 	switch(arp.mode)
 	{
-	case amUpDown:
-	case amAssign:
-		do
-			arp.noteIndex=(arp.noteIndex+1)%ARP_NOTE_MEMORY;
-		while(arp.notes[arp.noteIndex]==ASSIGNER_NO_NOTE);
-		break;
-		
-	case amRandom:
-		do
-			arp.noteIndex=random()%ARP_NOTE_MEMORY;
-		while(arp.notes[arp.noteIndex]==ASSIGNER_NO_NOTE);
-		break;
-	default:
-		return;
+        case amUpDown:
+            do
+                arp.noteIndex=(arp.noteIndex+1)%ARP_NOTE_MEMORY; // cycle through the array
+            while (arp.notes[arp.noteIndex]==ASSIGNER_NO_NOTE || (arp.previousIndex==(ARP_LAST_NOTE-arp.noteIndex))); // add here the avoidance of top/bottom note double playing
+            break;
+        case amAssign:
+            do
+                arp.noteIndex=(arp.noteIndex+1)%ARP_NOTE_MEMORY;
+            while(arp.notes[arp.noteIndex]==ASSIGNER_NO_NOTE);
+            break;
+
+        case amRandom:
+            step=0;
+            for (n=0;n<ARP_NOTE_MEMORY;n++)
+            {
+                if (arp.notes[n]!=ASSIGNER_NO_NOTE) step++;
+            }
+            // n is the random number of notes that will be skipped. Step is always >0 if there is more than one note but alsoways =0 if there is exactly one note
+            n=0;
+            if (step>1)
+			{
+				n=(random()%(step-1))+1;
+			}
+			if (arp.noteIndex<0) arp.noteIndex=0;
+            while(arp.notes[arp.noteIndex]==ASSIGNER_NO_NOTE || n>0)
+            {
+                arp.noteIndex=(arp.noteIndex+1)%ARP_NOTE_MEMORY;
+                if (arp.notes[arp.noteIndex]!=ASSIGNER_NO_NOTE) n--;
+            }
+            break;
+        default:
+            return;
 	}
 	
 	n=arp.notes[arp.noteIndex]&~ARP_NOTE_HELD_FLAG;
@@ -241,6 +268,7 @@ void arp_update(void)
 	
 	arp.previousNote=arp.notes[arp.noteIndex];
 	arp.previousTranspose=arp.transpose;
+	arp.previousIndex=arp.noteIndex;
 }
 
 void arp_init(void)
