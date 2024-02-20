3ac9b08f4f4aaba98dfb5a37d4c3ed22c9392841
GliGli <sfx.gligli@free.fr>
2024-02-21 00:24:28 +0100

    version: proper bootloader name

--- firmware_17xx/system/version.c
+++ firmware_17xx/system/version.c
@@ -1,3 +1,3 @@
 const char *synthName="GliGli's OverCycler";
-const char *synthBLName="GliGli's OverCycler";
+const char *synthBLName="GliGli's BootLoader";
 const char *synthVersion="Build " __DATE__ " " __TIME__;
