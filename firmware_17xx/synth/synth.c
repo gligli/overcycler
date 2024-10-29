gligli/overcycler

commit feb84889adf2b5ca16189f810d6e66c3d21cf705

synth: refreshTunedCVs fixes (by default, rest the synth on scale middle; code speed bugfix)

firmware_17xx/synth/synth.c
@@ -175,51 +175,51 @@ static void refreshTunedCVs(void)
 	
 	int32_t add;
 	
+	// get raw values
+
+	mTuneRaw=currentPreset.continuousParameters[cpMasterTune];
+	detuneRaw=currentPreset.continuousParameters[cpDetune];
+	baseCutoffRaw=currentPreset.continuousParameters[cpCutoff];
+	baseAPitch=currentPreset.continuousParameters[cpAFreq]>>2;
+	baseBPitch=currentPreset.continuousParameters[cpBFreq]>>2;
+	unisonDetuneRaw=currentPreset.continuousParameters[cpUnisonDetune];
+	trackRaw=currentPreset.continuousParameters[cpFilKbdAmt];
+	chrom=currentPreset.steppedParameters[spChromaticPitch];
+
+	// compute for oscs & filters
+
+	mTune=(mTuneRaw>>7)+INT8_MIN*2;
+	detune=(detuneRaw>>8)+INT8_MIN;
+	baseCutoff=((uint32_t)baseCutoffRaw*144)>>8; // tuning in C
+
+	baseCutoffNote=baseCutoff>>8;
+	baseANote=baseAPitch>>8; // 64 semitones
+	baseBNote=baseBPitch>>8;
+
+	baseCutoff&=0xff;
+
+	if(chrom>0)
+	{
+		baseAPitch=0;
+		baseBPitch=0;
+
+		if(chrom>1)
+		{
+			baseANote-=baseANote%12;
+			baseBNote-=baseBNote%12;
+		}
+	}
+	else
+	{
+		baseAPitch&=0xff;
+		baseBPitch&=0xff;
+	}
+
 	for(v=0;v<SYNTH_VOICE_COUNT;++v)
 	{
-		if (!assigner_getAssignment(v,&note))
-			continue;
+		note=MIDDLE_C_NOTE; // by default, rest the synth on scale middle (prevents analog glitches)
+		assigner_getAssignment(v,&note);
 		
-		// get raw values
-
-		mTuneRaw=currentPreset.continuousParameters[cpMasterTune];
-		detuneRaw=currentPreset.continuousParameters[cpDetune];
-		baseCutoffRaw=currentPreset.continuousParameters[cpCutoff];
-		baseAPitch=currentPreset.continuousParameters[cpAFreq]>>2;
-		baseBPitch=currentPreset.continuousParameters[cpBFreq]>>2;
-		unisonDetuneRaw=currentPreset.continuousParameters[cpUnisonDetune];
-		trackRaw=currentPreset.continuousParameters[cpFilKbdAmt];
-		chrom=currentPreset.steppedParameters[spChromaticPitch];
-
-		// compute for oscs & filters
-
-		mTune=(mTuneRaw>>7)+INT8_MIN*2;
-		detune=(detuneRaw>>8)+INT8_MIN;
-		baseCutoff=((uint32_t)baseCutoffRaw*144)>>8; // tuning in C
-
-		baseCutoffNote=baseCutoff>>8;
-		baseANote=baseAPitch>>8; // 64 semitones
-		baseBNote=baseBPitch>>8;
-
-		baseCutoff&=0xff;
-
-		if(chrom>0)
-		{
-			baseAPitch=0;
-			baseBPitch=0;
-
-			if(chrom>1)
-			{
-				baseANote-=baseANote%12;
-				baseBNote-=baseBNote%12;
-			}
-		}
-		else
-		{
-			baseAPitch&=0xff;
-			baseBPitch&=0xff;
-		}
-
 		// oscs
 		
 		add=getStaticCV(cvAPitch)+mTune-(detune>>1);

commit 8bf99e18c3acd598dc58bfc9cb8de1e33823469b

synth: glide bugfix; only do "rest the synth on scale middle" at init this fixes a bug introduced in commit feb84889adf2b5ca16189f810d6e66c3d21cf705

firmware_17xx/synth/synth.c
@@ -217,8 +217,8 @@ static void refreshTunedCVs(void)
 
 	for(v=0;v<SYNTH_VOICE_COUNT;++v)
 	{
-		note=MIDDLE_C_NOTE; // by default, rest the synth on scale middle (prevents analog glitches)
-		assigner_getAssignment(v,&note);
+		if(!assigner_getAssignment(v,&note))
+			continue;
 		
 		// oscs
 		
@@ -902,6 +902,14 @@ void synth_init(void)
 	lfo_init(&synth.lfo[0]);
 	lfo_init(&synth.lfo[1]);
 
+	// rest the synth on scale middle (prevents analog glitches)
+	for(i=0;i<SYNTH_VOICE_COUNT;++i)
+	{
+		synth.oscANoteCV[i]=synth.oscATargetCV[i]=tuner_computeCVFromNote(i,MIDDLE_C_NOTE,0,cvAPitch);
+		synth.oscBNoteCV[i]=synth.oscBTargetCV[i]=tuner_computeCVFromNote(i,MIDDLE_C_NOTE,0,cvBPitch);
+		synth.filterNoteCV[i]=synth.filterTargetCV[i]=tuner_computeCVFromNote(i,MIDDLE_C_NOTE,0,cvCutoff);
+	}
+	
 	// load settings from storage & load static stuff
 
 	settings_load();